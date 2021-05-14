6b3115dc4e9a5344f6db9f5e776c2a1328557e67
Shiwei Wang <38160380+AirWSW@users.noreply.github.com>
2021-05-14 10:54:41 +0800

    add rules formula

--- .github/unattended-upgrades.sh
+++ .github/unattended-upgrades.sh
@@ -6,21 +6,18 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-patch_v2ray_core() {
-    wget -P .cache/v2ray-core https://github.com/v2fly/v2ray-core/archive/$FILENAME
-    sed -i "s/^  url \"https:\/\/github.com\/v2fly\/v2ray-core\/archive\/.*\"$/  url \"https:\/\/github.com\/v2fly\/v2ray-core\/archive\/$FILENAME\"/g" Formula/v2ray.rb
-    SHA256SUM=$(sha256sum .cache/v2ray-core/$FILENAME | awk '{print $1}')
-    sed -i "s/^  sha256 \".*\" # latest.tar.gz$/  sha256 \"$SHA256SUM\" # latest.tar.gz/g" Formula/v2ray.rb
-    cat Formula/v2ray.rb | grep "# latest.tar.gz"
+patch_v2ray_rules_version() {
+    sed -i "s/^  version \".*\"$/  version \"$VERSION\"/g" Formula/v2ray-rules.rb
+    cat Formula/v2ray-rules.rb | grep "# latest.tar.gz"
 }
 
-FILENAME=$(get_latest_release v2fly/v2ray-core).tar.gz patch_v2ray_core
+VERSION=$(get_latest_release Loyalsoldier/v2ray-rules-dat).tar.gz patch_v2ray_rules_version
 
 patch_v2ray_rules_dat() {
     wget -P .cache/v2ray-rules-dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/$FILENAME
     SHA256SUM=$(sha256sum .cache/v2ray-rules-dat/$FILENAME | awk '{print $1}')
-    sed -i "s/^    sha256 \".*\" # $FILENAME$/    sha256 \"$SHA256SUM\" # $FILENAME/g" Formula/v2ray.rb
-    cat Formula/v2ray.rb | grep "# $FILENAME"
+    sed -i "s/^    sha256 \".*\" # $FILENAME$/    sha256 \"$SHA256SUM\" # $FILENAME/g" Formula/v2ray-rules.rb
+    cat Formula/v2ray-rules.rb | grep "# $FILENAME"
 }
 
 FILENAME=apple-cn.txt        patch_v2ray_rules_dat
@@ -37,6 +34,3 @@ FILENAME=reject-list.txt     patch_v2ray_rules_dat
 FILENAME=win-extra.txt       patch_v2ray_rules_dat
 FILENAME=win-spy.txt         patch_v2ray_rules_dat
 FILENAME=win-update.txt      patch_v2ray_rules_dat
-
-DATE=$(date +%Y%m%d)
-sed -i "s/^  revision .*$/  revision $DATE/g" Formula/v2ray.rb
---
+++ Formula/v2ray-rules.rb
@@ -0,0 +1,107 @@
+class V2rayRules < Formula
+  version "0"
+
+  depends_on formula: "v2ray"
+
+  resource "apple-cn" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/apple-cn.txt"
+    sha256 "8857b2d96289a90b42699ebdbc7053baf7a7b23b99b7fee508a793784e1af9b1" # apple-cn.txt
+  end
+  resource "direct-list" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/direct-list.txt"
+    sha256 "c4eb8b40f779efd05d37e40dd5ba73ca40bb4c1fa55a19d7d107028a94c4cd7b" # direct-list.txt
+  end
+  resource "direct-tld-list" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/direct-tld-list.txt"
+    sha256 "6586d9029edd42ca9d3e8a32d2d8c3aced37c9228615a5e693212b935a4d8efd" # direct-tld-list.txt
+  end
+  resource "geoip" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+    sha256 "8769ff4b9650c38e45820441127b1c962aeb490e6d49ed25f00d16fb8076ce58" # geoip.dat
+  end
+  resource "geosite" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+    sha256 "14f05bfc954bcfad3b193acaa2d129bec933eeff5352ce8ae7f3555a32690975" # geosite.dat
+  end
+  resource "gfw" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/gfw.txt"
+    sha256 "832d661e299d4dd7cf48687d3f8bc9c05b21c93f0af54b7238569104897b599e" # gfw.txt
+  end
+  resource "google-cn" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/google-cn.txt"
+    sha256 "555f0be3aa2f262ff88ffaffeaa9c5d8881e995bb1ae7e552779c374ab024f90" # google-cn.txt
+  end
+  resource "greatfire" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/greatfire.txt"
+    sha256 "1fcbb0a9c5022acd7ca7e252a2af6cc756ffecc1059986f19d92ec904e0381cc" # greatfire.txt
+  end
+  resource "proxy-list" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/proxy-list.txt"
+    sha256 "4356480821044766db29949df49088cccdee9a77fd62c22fb520aea123ee9813" # proxy-list.txt
+  end
+  resource "proxy-tld-list" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/proxy-tld-list.txt"
+    sha256 "8590a159c76481623718f9c383d18423bc6ffcc8542e5419c70802f34c891fcb" # proxy-tld-list.txt
+  end
+  resource "reject-list" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/reject-list.txt"
+    sha256 "6ff74e9d70f1233908a24294143023e658d49b8c16c4153bee3b7c3c2e27626d" # reject-list.txt
+  end
+  resource "win-extra" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/win-extra.txt"
+    sha256 "34f38a3c30f89463c708f42e51b07bf2bb8ae2e23fffa15a52c5badcc89be21d" # win-extra.txt
+  end
+  resource "win-spy" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/win-spy.txt"
+    sha256 "df8574fece846b8e9aba9ee57ec73ee75dc8c09337d343e51070da45c712eaca" # win-spy.txt
+  end
+  resource "win-update" do
+    url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/win-update.txt"
+    sha256 "d0cd352c0f95331d95e22ea2d9947f0424c4740b5acb06d1661d8b88001a11ef" # win-update.txt
+  end
+
+  def install
+    resource("apple-cn").stage do
+      pkgshare.install "apple-cn.txt"
+    end
+    resource("direct-list").stage do
+      pkgshare.install "direct-list.txt"
+    end
+    resource("direct-tld-list").stage do
+      pkgshare.install "direct-tld-list.txt"
+    end
+    resource("geoip").stage do
+      pkgshare.install "geoip.dat"
+    end
+    resource("geosite").stage do
+      pkgshare.install "geosite.dat"
+    end
+    resource("gfw").stage do
+      pkgshare.install "gfw.txt"
+    end
+    resource("google-cn").stage do
+      pkgshare.install "google-cn.txt"
+    end
+    resource("greatfire").stage do
+      pkgshare.install "greatfire.txt"
+    end
+    resource("proxy-list").stage do
+      pkgshare.install "proxy-list.txt"
+    end
+    resource("proxy-tld-list").stage do
+      pkgshare.install "proxy-tld-list.txt"
+    end
+    resource("reject-list").stage do
+      pkgshare.install "reject-list.txt"
+    end
+    resource("win-extra").stage do
+      pkgshare.install "win-extra.txt"
+    end
+    resource("win-spy").stage do
+      pkgshare.install "win-spy.txt"
+    end
+    resource("win-update").stage do
+      pkgshare.install "win-update.txt"
+    end
+  end
+end
